2f7063d29807ecdf6ddcaeaa3bb8b0759492c15f
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-13 08:26:26 -0300

    refactorization of fetch-plugin

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,13 +11,54 @@ export const fetchPlugin = (inputCode: string) => {
     return{
         name: "fetch-plugin",
         setup(build: esbuild.PluginBuild){
-            build.onLoad({ filter: /.*/ }, async (args: any): Promise<any> => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode,
-                    };
+
+            build.onLoad({filter: /(^index\.js$)/}, () => {
+                return {
+                    loader: 'jsx',
+                    contents: inputCode,
+                };
+            })
+
+            build.onLoad({filter: /.css$/}, async(args:any): Promise<any> => {
+                // Check to see if we have already fetched this file
+                // and if it is in the cache, return immediately
+
+                const cachedResult = await fileCache.getItem(args.path);
+
+                if(cachedResult){
+                    return cachedResult;
                 }
+
+                const { data, request } = await axios.get(args.path)
+
+
+                console.log(request);
+                console.log(data);
+
+
+                // @ts-ignore
+                const escaped = data.replace(/\n/g,'').replace(/"/g,'\\"').replace(/'/g,"\\'");
+
+                const contents =
+                    `
+                        const style = document.createElement('style');
+                        style.innerText = '${escaped}';
+                        document.head.appendChild(style);
+                    `
+                const result = {
+                    loader: "jsx",
+                    contents,
+                    resolveDir: new URL("./",request.responseURL).pathname
+                }
+
+                await fileCache.setItem(args.path,result)
+
+                return result;
+            })
+
+
+            build.onLoad({ filter: /.*/ }, async (args: any): Promise<any> => {
+
 
                 // Check to see if we have already fetched this file
                 // and if it is in the cache, return immediately
@@ -34,20 +75,13 @@ export const fetchPlugin = (inputCode: string) => {
                 console.log(request);
                 console.log(data);
 
-                const fileType = args.path.match(/.css$/) ? "css" : "jsx";
 
                 // @ts-ignore
-                const escaped = data.replace(/\n/g,'').replace(/"/g,'\\"').replace(/'/g,"\\'");
 
-                const contents = fileType === "css" ?
-                    `
-                        const style = document.createElement('style');
-                        style.innerText = '${escaped}';
-                        document.head.appendChild(style);
-                    ` : data;
+
                 const result = {
                     loader: "jsx",
-                    contents,
+                    contents: data,
                     resolveDir: new URL("./",request.responseURL).pathname
                 }
 
