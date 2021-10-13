615640c09ef1bb913803b0c4b3589d556f17721f
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-13 12:48:56 -0300

    basic functional monaco code editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,11 +2,18 @@ import MonacoEditor from "@monaco-editor/react";
 
 interface CodeEditorProps {
     initialValue: string;
-
+    onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({initialValue}) => {
-    return <MonacoEditor value={initialValue} language="javascript" height="500px" theme="dark" options={{wordWrap: "on", minimap: {enabled: false}, showUnused: false, folding: false, lineNumbersMinChars: 3, fontSize: 16, scrollBeyondLastLine: false, automaticLayout: true }}/>;
+const CodeEditor: React.FC<CodeEditorProps> = ({initialValue,onChange}) => {
+    const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+        monacoEditor.onDidChangeModelContent(() => {
+            onChange(getValue());
+        })
+    };
+
+
+    return <MonacoEditor editorDidMount={onEditorDidMount} value={initialValue} language="javascript" height="500px" theme="dark" options={{wordWrap: "on", minimap: {enabled: false}, showUnused: false, folding: false, lineNumbersMinChars: 3, fontSize: 16, scrollBeyondLastLine: false, automaticLayout: true }}/>;
 };
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -70,7 +70,7 @@ const App = () => {
 `;
 
     return <div>
-        <CodeEditor initialValue="const a=1;"/>
+        <CodeEditor initialValue="const a=1;" onChange={(value) => setInput(value)}/>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}>Submit</button>
