24a50b658d032748f1a80fa8b414987bffdc6c49
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-14 15:01:48 -0300

    added prettier!

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from "react";
 import MonacoEditor,{EditorDidMount} from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
     initialValue: string;
@@ -6,14 +9,41 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({initialValue,onChange}) => {
+    const editorRef = useRef<any>();
     const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+        editorRef.current = monacoEditor;
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue());
         })
+        monacoEditor.getModel()?.updateOptions({tabSize: 2});
     };
 
+    const onFormatClick = () => {
+        // TODO: get current value from editor
+        const unformatted = editorRef.current.getModel().getValue();
 
-    return <MonacoEditor editorDidMount={onEditorDidMount} value={initialValue} language="javascript" height="500px" theme="dark" options={{wordWrap: "on", minimap: {enabled: false}, showUnused: false, folding: false, lineNumbersMinChars: 3, fontSize: 16, scrollBeyondLastLine: false, automaticLayout: true }}/>;
+        // TODO: format that value
+
+        const formatted = prettier.format(unformatted,{
+            parser: "babel",
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true
+        });
+
+        // TODO: set the formatted value back in the editor
+
+        editorRef.current.setValue(formatted);
+    }
+
+
+    return (
+        <div>
+            <button onClick={onFormatClick}>Format</button>
+            <MonacoEditor editorDidMount={onEditorDidMount} value={initialValue} language="javascript" height="500px" theme="dark" options={{wordWrap: "on", minimap: {enabled: false}, showUnused: false, folding: false, lineNumbersMinChars: 3, fontSize: 16, scrollBeyondLastLine: false, automaticLayout: true }}/>
+        </div>
+    )
 };
 
 export default CodeEditor;
