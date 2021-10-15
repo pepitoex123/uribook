bc826705bc777577d0fadd8cda4ffe3ca41be8ab
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-15 16:02:22 -0300

    added basic horizontal handler

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
 interface ResizableProps {
@@ -6,13 +6,28 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({direction,children}) => {
+    let resizableProps: ResizableBoxProps;
+
+    if(direction === "horizontal"){
+        resizableProps = {
+            height: Infinity,
+            width: window.innerWidth * 0.75,
+            resizeHandles: ["e"],
+            maxConstraints: [window.innerWidth * 0.75,Infinity],
+            minConstraints: [window.innerWidth * 0.2,Infinity]
+        }
+    }else{
+        resizableProps = {
+            height: 300,
+            width: Infinity,
+            resizeHandles: ["s"],
+            maxConstraints: [Infinity,window.innerHeight * 0.9],
+            minConstraints: [Infinity,50]
+        }
+    }
+
     return (
-        <ResizableBox height={300}
-                      width={Infinity}
-                      resizeHandles={["s"]}
-                      maxConstraints={[Infinity,window.innerHeight * 0.9]}
-                      minConstraints={[Infinity,50]}
-        >
+        <ResizableBox {...resizableProps}>
             {children}
         </ResizableBox>
     )
