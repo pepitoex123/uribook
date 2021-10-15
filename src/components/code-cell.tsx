860eb0d5945abaa868f0900f4e4c7c2e2031900b
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-15 17:25:35 -0300

    fixed issue in which iframe wouldn't update correctly

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -12,12 +12,17 @@ const CodeCell = () => {
     const [input,setInput] = useState("");
     const [code,setCode] = useState("");
 
+    useEffect(() => {
+        const timer = setTimeout(async() => {
+            const output = await bundle(input);
+            setCode(output);
+        },1000)
 
+        return () => {
+            clearTimeout(timer);
+        };
+    },[input])
 
-    const onClick = async() => {
-        const output = await bundle(input);
-        setCode(output);
-    }
 
 
 
