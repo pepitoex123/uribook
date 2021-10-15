9a8b42e9791bf0b0ea54f7c167a4d63d2bf1e05e
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-15 17:12:42 -0300

    finished resizable component

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -10,6 +10,7 @@ const Resizable: React.FC<ResizableProps> = ({direction,children}) => {
     let resizableProps: ResizableBoxProps;
     const [innerHeight,setInnerHeight] = useState(window.innerHeight);
     const [innerWidth,setInnerWidth] = useState(window.innerWidth);
+    const [width,setWidth] = useState(window.innerWidth * 0.75);
 
     useEffect(() => {
 
@@ -22,6 +23,9 @@ const Resizable: React.FC<ResizableProps> = ({direction,children}) => {
             timer = setTimeout(() => {
                 setInnerHeight(window.innerHeight);
                 setInnerWidth(window.innerWidth);
+                if(window.innerWidth * 0.75 < width){
+                    setWidth(window.innerWidth * 0.75);
+                }
             },100);
         }
         window.addEventListener("resize",listener);
@@ -29,16 +33,19 @@ const Resizable: React.FC<ResizableProps> = ({direction,children}) => {
         return () => {
             window.removeEventListener("resize",listener);
         }
-    },[])
+    },[width])
 
     if(direction === "horizontal"){
         resizableProps = {
             className: "resize-horizontal",
             height: Infinity,
-            width: innerWidth * 0.75,
+            width,
             resizeHandles: ["e"],
             maxConstraints: [innerWidth * 0.75,Infinity],
-            minConstraints: [innerWidth * 0.2,Infinity]
+            minConstraints: [innerWidth * 0.2,Infinity],
+            onResizeStop: (event,data) => {
+                setWidth(data.size.width);
+            }
         }
     }else{
         resizableProps = {
